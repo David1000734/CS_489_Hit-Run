72b8eeb94f489e1e51ea1f0370b7212d616104e7
David <lam.david459@yahoo.com>
2024-10-11 10:46:43 -0700

    Car is moving and gap following
    Added Steering Angle calculator function.
    Slight improvement to best point function
    Speed is used from the parameters
    Added helper function
    Allowed the car to drive towards the target "best" angle

--- sim_ws/src/gap_follow/src/reactive_node.cpp
+++ sim_ws/src/gap_follow/src/reactive_node.cpp
@@ -48,6 +48,7 @@ public:
     }
 
 private:
+    const double PI = 3.1415926535;
     std::string lidarscan_topic = "/scan";
     std::string drive_topic = "/drive";
 
@@ -98,28 +99,60 @@ private:
         return;
     }
 
+    /// @brief Funciton will calculate the target degrees to reach each degree
+    /// Presumable able to handle different size arrays but untested.
+    ///
+    /// @param arr_size Size of the array it's searching for
+    ///
+    /// @param target_index The index we want to steer towards.
+    ///
+    /// @return The angle to steer to reach the target. In DEGREES.
+    ///
+    /// @note This function assumes that the angle from one index
+    /// to the next is 2 degrees. 
+    double get_steering_angle(int arr_size, int target_index) {
+        // Since we are working with a size that may change
+        // We will need to find out what our min and max angles are.
+        //
+        // To do this, recall that we know every index represents 2%
+        // Thus the size / 2 would get us the full angle pos and neg
+        //
+        // If we take that (size / 2) / 2, this would get us the
+        // positive range of our angles. Thus our angle is +- that value
+        double center = arr_size / 2;         // Center of the angles
+        double range_angle = arr_size / 4;      // Dividing by 2 twice. Just divide by 4
+
+        // To get to our target index, all we do is target - center
+        // Assume we have a range of 100, target is 80
+        // Our center is 50, to get from 50 to 80, we need to
+        //     move 30 index to the right. This translates to 60 degrees
+        // Assume center is 50 and target is 40
+        //     We need to move 20 idx to the left. Which is -20 degrees
+
+        return (center - target_index);
+    }
+
     int find_best_point(std::map<int, double> gap)
     {
         int index = 0;          // Target index
-        double largest = 0.0;   // Max value
 
         // Start_i & end_i are start and end indicies of max-gap range, respectively
 
         // Pick the best point by finding the furthest one
         std::map<int, double>::iterator itr;
         for (itr = gap.begin(); itr != gap.end(); itr++) {
-            if (itr -> second > largest) {
+            if (itr -> second > gap[index]) {
                 // Take the index of the largest value
-                largest = itr -> first;
+                index = itr -> first;
             }
         }
 
-        RCLCPP_INFO(
-            this -> get_logger(),
-            "\nBest point IDX: %f\t Value: %f.\n",
-            largest,
-            gap[largest])
-        ;
+        // RCLCPP_INFO(
+        //     this -> get_logger(),
+        //     "\nBest point IDX: %f\t Value: %f.\n",
+        //     largest,
+        //     gap[largest])
+        // ;
 
         // Return index of best point in ranges
 	    // Naive: Choose the furthest point within ranges and go there
@@ -131,6 +164,8 @@ private:
     {
         double distance_threshold = this -> get_parameter("dist").as_double();
         int gap_size = this -> get_parameter("gap").as_int();
+        double speed = this -> get_parameter("speed").as_double();
+
         /*
         RCLCPP_INFO(
             this -> get_logger(), 
@@ -144,6 +179,7 @@ private:
         std::vector<float> range_data = this -> preprocess_lidar(scan_msg -> ranges);
         std::map<int, double> largest_gap;      // Global Scope
         std::map<int, double> temp_gap;         // Local Scope
+        double steering_angle = 0.0;
 
         for (int i = 0; i < range_data.size(); i++) {
             // Check the curent point, if it meets our requirements, do stuff
@@ -170,15 +206,64 @@ private:
 
         //largest gap
         /*
+        // DEBUG
         for (auto index : largest_gap){
             RCLCPP_INFO(this -> get_logger(),
             "\nTemp-Idx: %i\tTemp-Value: %f.\n",
             index.first,
             index.second);
         }
-        */
-       this -> find_best_point(largest_gap);
         
+        // DEBUG
+        int temp = this -> find_best_point(largest_gap);
+        RCLCPP_INFO(this -> get_logger(),
+            "\nTarget: %i\tAngle: %f\n",
+            temp,
+            get_steering_angle(scan_msg -> ranges.size(), temp)
+        );
+
+        // DEBUG
+        for (int i = -20; i < 40; i += 10) {
+            RCLCPP_INFO(this -> get_logger(),
+                "\nTarget: %f\tAngle: %f\n",
+                i,
+                get_steering_angle(scan_msg -> ranges, i)
+            );
+        }
+        */
+        int target_idx = this -> find_best_point(largest_gap);
+        steering_angle = get_steering_angle(
+                        scan_msg -> ranges.size(),
+                        target_idx
+        );
+
+        // steering_angle = get_steering_angle(
+        //         scan_msg -> ranges.size(),
+        //         find_best_point(largest_gap)
+        // );
+        steering_angle *= -1;       // Turn away from walls.
+        
+        // steering_angle = get_steering_angle(
+        //                     scan_msg -> ranges.size(),
+        //                     find_best_point(largest_gap)
+        // );
+
+        // If the steering angle is between 0 degrees and 10 degrees, the car should drive at 1.5 meters per second.
+        // If the steering angle is between 10 degrees and 20 degrees, the speed should be 1.0 meters per second.
+        // Otherwise, the speed should be 0.5 meters per second.
+        if (steering_angle > -10.0001 && steering_angle < 10.0001){
+            // speed = speed
+
+        // If less than 20 and greater than 10
+        } else if ((steering_angle > 9.999 && steering_angle < 20.0001) ||
+                   (steering_angle > -10.0001 && steering_angle < -20.0001)) {
+            speed = 1.0;
+        
+        // Any other steering angle, speed is 0.5
+        } else {
+            speed = 0.5;
+        }
+
         // Process each LiDAR scan as per the Follow Gap algorithm & publish an AckermannDriveStamped Message
 
         /// TODO:
@@ -191,9 +276,17 @@ private:
         // Find the best point in the gap 
 
         // Publish Drive message
-        // publish_ackerman(2.5, 1.9);
+        publish_ackerman(speed, degree_to_radian(steering_angle));
     }
 
+    /// @brief Helper function to post the speed and angle for the car
+    /// 
+    /// @param car_speed Specified speed for the car. Nullable
+    ///
+    /// @param steer_angle Specified angle. Nullable
+    ///
+    /// @param debug True if you want to print the
+    /// speed and angle being published. Default is false.
     void publish_ackerman(const double car_speed = 0.0, const double steer_angle = 0.0, const bool debug = false) {
         ackermann_msgs::msg::AckermannDriveStamped acker_message = ackermann_msgs::msg::AckermannDriveStamped();
 
@@ -219,6 +312,11 @@ private:
                 acker_message.drive.speed, acker_message.drive.steering_angle
             );
         }
+    }
+
+    double degree_to_radian(double degrees) {
+        // Helper function, change degrees to radian.
+        return (degrees * (PI / 180));
     }
 };
 
