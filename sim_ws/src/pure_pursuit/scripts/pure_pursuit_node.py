be85e3c37c3c503f25a14b46f7da1652af740601
David <lam.david459@yahoo.com>
2024-10-29 18:10:42 -0700

    Added ranges_libic
    Added the ranges thing
    Started adding stuff into python

--- sim_ws/src/pure_pursuit/scripts/pure_pursuit_node.py
+++ sim_ws/src/pure_pursuit/scripts/pure_pursuit_node.py
@@ -5,6 +5,8 @@ from rclpy.node import Node
 import numpy as np
 from sensor_msgs.msg import LaserScan
 from ackermann_msgs.msg import AckermannDriveStamped, AckermannDrive
+from nav_msgs.msg import Odometry
+
 # TODO CHECK: include needed ROS msg type headers and libraries
 
 class PurePursuit(Node):
@@ -14,7 +16,39 @@ class PurePursuit(Node):
     """
     def __init__(self):
         super().__init__('pure_pursuit_node')
+
+        self.get_logger().info('Python Hey.')
+
+        # Declare parameters
+        self.declare_parameter('speed', float(0))
+
         # TODO: create ROS subscribers and publishers
+
+        self.acker_publisher = self.create_publisher(
+            AckermannDriveStamped,
+            'drive',
+            10
+        )
+
+        self.acker_subscription = self.create_subscription(
+            AckermannDriveStamped,
+            'drive',
+            self.acker_callback,
+            10
+        )
+
+        self.pose_subscription = self.create_subscription(
+            Odometry,
+            '/pf/pose/odom',
+            self.pose_callback,
+            10
+        )
+
+        self.get_logger().info('Hello from Python.')
+
+    def acker_callback(self, acker_msg): 
+
+        pass
 
     def pose_callback(self, pose_msg):
         pass
@@ -25,6 +59,29 @@ class PurePursuit(Node):
         # TODO: calculate curvature/steering angle
 
         # TODO: publish drive message, don't forget to limit the steering angle.
+
+    def publish_ackerman(self, car_speed = 0.0, steering = 0.0, debug = false):
+        if (debug):
+            self.get_logger().info(
+                "\n********** Try Publish **********\n" +
+                "Speed: %f\tSteering_Angle: %f\n",
+                car_speed,
+                steering
+            )
+
+        ack_msg = AckermannDriveStamped()
+
+        ack_msg.drive.speed = float(car_speed)
+        ack_msg.drive.steering_angle = float(steering)
+        self.acker_publisher.publish(ack_msg)
+
+        if (debug):
+            self.get_logger().info(
+                "\n********** Published **********\n" +
+                "Speed: %f\tSteering_Angle: %f\n",
+                ack_msg.drive.speed,
+                ack_msg.drive.steering_angle
+            )
 
 def main(args=None):
     rclpy.init(args=args)
